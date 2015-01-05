f79044ec4d3c79b541b7a81fd4b6e296ebd09e21
somabc <pi@ee-ras2.(none)>
2015-01-05 12:05:30 +0000

    ex1 extra credit

--- ex1.c
+++ ex1.c
@@ -1,6 +1,13 @@
+#include <stdio.h>
+
 int main(int argc, char *argv[])
 {
     puts("Hello world.");
-
+    puts(" ");
+    puts("In the beginning was assembly");
+    puts("but who wants to use that");
+    puts("So we have C");
+    puts("and it is good.");
     return 0;
 }
+
